045b90d1be149bb205a050bcc0a9615b7f1252f1
Ajblander <ajblander@uchicago.edu>
2025-05-27 00:09:00 -0500

    Revert "add database folder, instructions in readme.txt"
    This reverts commit ab432c26ff31846a507a77553c835898610e5015.

--- database/clean-data/get_kaggle_dataset.py
+++
@@ -1,9 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("NUFORC/ufo-sightings")
-
-print("Path to dataset files:", path)
-
-# to move:
-# mv ~/.cache/kagglehub/datasets/NUFORC/ufo-sightings/versions/2 [YOUR PATH HERE]
